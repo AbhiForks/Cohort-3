3dc3b1171fd98a945e81f3c7059e9f29f70b9228
Abhilash <abhilashvishwa12@gmail.com>
2025-05-29 12:11:32 +0530

    latest

---
+++ JavaScript/1.2.js
@@ -0,0 +1,36 @@
+// Syntax of Javascript
+
+// let firstName = "abhi"
+// const dateOfBirth = "12-12-2004"
+// // var dateOfBirth = "12-12-2004"
+// // let dateOfBirth = "12-12-2004"
+// var age = "20"
+
+// console.log(dateOfBirth);
+// console.log(firstName);
+// console.log(age);
+
+// let firstName = "abhi"
+// firstName = "achu"
+// firstName = "rocky"
+// console.log(firstName);
+
+
+// Assignment
+
+// let colour = "black"
+// const height = "179cm"
+// var doYouLikePizza = "yes"
+
+// console.log(colour);
+// console.log(height);
+// console.log(doYouLikePizza);
+
+// functions
+
+function greet(firstName) {
+  return "Hello " + firstName
+}
+let ans = greet ("abhilash")
+
+console.log(ans);
