b34c1a84b20108a7ffa486866bc5f4bd0ca986e8
Abhilash <abhilashvishwa12@gmail.com>
2025-06-04 10:50:19 +0530

    basics

--- JavaScript/1.2.js
+++ JavaScript/1.2.js
@@ -51,14 +51,66 @@
 //   console.log("You are not eligible to vote");
 // }
 
-function canVote (age) {
-  if (age > 18) {
-    return true;
-  }else {
-    return false;
-  }
-}
-let ans = canVote(19)
+// function canVote (age) {
+//   if (age > 18) {
+//     return true;
+//   }else {
+//     return false;
+//   }
+// }
+// let ans = canVote(19)
 
-console.log(ans);
+// console.log(ans);
+
+// function canVote (age) {
+//   if (age > 18){
+//     return true;
+//   }else {
+//     return false;
+//   }
+// }
+// let ans = canVote(20)
+// console.log(ans);
+
+// let users = ["abhilash", "achu", "rocky", "abhi", "Achu"]
+
+// for (let i = 0; i < 4; i ++){
+//   console.log( users[i]);
+  
+// }
+
+// let user = ["abhi", "abi", "abhi2", "abi2", "abhi3", "abi", "abi4","achu"," achu "]
+
+// let usersUpdate = user.length
+
+// for (let i = 0; i < user.length; i ++){
+//   console.log(user [i]);
+  
+// }
+
+/// if else Assignment
+// let num = 12;
+
+// if (num % 2 == 0) {
+//   console.log("this is even number");
+// } else {
+//   console.log("this is a odd number");
+// }
+
+//// loops Assignment
+// Write a function called sum that finds the sum from 1 to a number
+function sum(num) {
+  // initialize sum to 0
+  let sum = 0;
+
+  // loop from 1 to num
+  for (let i = 1; i <= num; i++) {
+    // add i to sum
+    sum += i;
+  for (i = 0; i > sum; i= i + sum ){
+  }
+
+  // return sum
+  return sum;
+}
 
