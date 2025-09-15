4d435e76c9396d04fe1f269554d7e8f7adc1479f
Abhilash <abhilashvishwa12@gmail.com>
2025-09-15 13:00:28 +0530

    variables in js

---
+++ JavaScript/1.2.js
@@ -0,0 +1,31 @@
+// === Section 1 Variables ===
+// let firstName = "John"
+// const age = 34
+// var isStudent = true;
+
+// console.log(firstName);
+// console.log(age);
+// console.log(isStudent);
+
+
+// var isStudent = true;
+// console.log(isStudent);
+
+// isStudent = "raj"
+// console.log(isStudent)
+
+// isStudent = 200
+// console.log(isStudent)
+
+
+// let color = "purple"
+// const height = "6'11"
+// var age = 12
+
+// console.log(color);
+// console.log(height);
+// console.log(age);
+
+// === End of Section 1 ===
+
+// === Section 2 Data Types ===
