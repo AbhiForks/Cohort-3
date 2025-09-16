1043fe88f6d0caf7f561c82355894e50b3d6d85c
Abhilash <abhilashvishwa12@gmail.com>
2025-09-16 12:09:39 +0530

    functions

--- JavaScript/1.2.js
+++ JavaScript/1.2.js
@@ -42,3 +42,71 @@
 // let result = square(5);  
 // console.log(result);  
 
+// function makeSandwich() {
+//   console.log("Bread");
+//   console.log("Peanut Butter");
+//   console.log("Jelly");
+//   console.log("Bread");
+// }
+// console.log(makeSandwich);
+// makeSandwich();
+
+// Assignment #1
+// Write a function sum that finds the sum of two numbers. 
+// Side quest - Try passing in a string instead of a number and see what happens?
+// function sum(a,b){
+//   console.log(a + b);
+// }
+// sum("great",5)
+
+// Assignment #2
+// Write a function called canVote that returns true or false if the age of a user is > 18
+// function canVote(age){
+//   return age > 18
+// }
+
+// console.log(canVote(20));
+
+// function canVote (age){
+//   console.log(age > 18)
+// }
+// canVote(10)
+
+// Function declaration
+// function greet(name) {
+//     return "Hello, " + name;
+// }
+
+// // Function call
+// let message = greet("John"); // "Hello, John"
+// console.log(message);
+
+//// IF ElSe
+
+// function canVote(age){
+//     if (age > 18){
+//         return true;
+//     }else{
+//         return false
+//     }
+// }
+
+// let legalAge = canVote(19)
+// console.log(legalAge);
+ 
+
+// This array contains a list of 10 user names
+// let users = ["abhi", "sarah", "michael", "emma", "james", "olivia", "john", "lisa", "david", "sophia"]
+
+// This loop will run 10 times (from 0 to 9)
+// Each time it runs, it prints one name from the users array
+// i is used to get each name one by one (like counting from 1st name to last name)
+// for (let i = 0; i < 10; i++){
+//   console.log(users[i])
+// }
+
+
+
+////// Complex types - objects
+
+
