b3f339af02916c6a7ead56981ba8b257fa700ff1
Abhilash <abhilashvishwa12@gmail.com>
2025-12-02 13:45:50 +0530

    day 1

--- JavaScript/Variables & Data Types.js
+++ JavaScript/Variables & Data Types.js
@@ -1,9 +1,45 @@
-// Variables
+// // Variables
 
-let name = "Abhilash"
-let age = 25 
+// let name = "Abhilash"
+// let age = 25 
+
+// console.log(name, age)
+
+// // Const cant be changed 2 times .
+
+// const account = 1234
+// account = 23;
+
+// console.log(account)
+
+// var can be changed several times and can be accessed outside the scope . but let is not like that so its used as a replacement of var .
+// var a = 10
+// var a = 20
+
+// console.log(a)
+
+
+//////  Data types in JS
+
+/////   Primitive Data types - number, string, boolean, undefined,null, bigint, symbol
+
+// number
+// let a = 10
+// let b = 2.45
+// console.log(a,b)
+
+// string
+// let c = "i am a maniac"
+// let d = 'hello'
+// console.log (c,d)
+
+// boolean
+// let login = true
+// let notLogin= false
+// console.log = (login)
+
+// undefined
 
 
 
-
-
+////  Non Primitive - array, object , Function 
